8921665151e644c30b19e78cf95cfd51fa4a34c4
Borche <borosoposki@yahoo.com>
2021-02-01 09:15:48 +0100

    redux store

---
+++ public/src/redux/store.js
@@ -0,0 +1,7 @@
+// vendor imports
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+// reducers
+import reducers from './ducks';
+
+export default createStore(reducers, applyMiddleware(createLogger()));
